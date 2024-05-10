96fd06efb127de3ef23f364eff541c7196fef0eb
Bais <nilanbais@outlook.com>
2024-05-10 17:17:43 +0200

    add js

---
+++ scr/js/_button.js
@@ -0,0 +1,40 @@
+// Scripts for all button related eventlisteners
+import { generatePassword } from './_password_generator.js'
+
+// The Viewbox where the result will be shown
+const result_viewbox = document.getElementById("result");
+
+// Selecting the Range Slider container which will effect the LENGTH property of the password.
+const slider = document.querySelector(".length__slider");
+
+// Checkboxes representing the options that is responsible to create differnt type of password based on user
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numberEl = document.getElementById("number");
+const symbolEl = document.getElementById("symbol");
+
+
+// Button to copy the text
+const copyBtn = document.getElementById("copy-btn");
+// Result viewbox container
+const resultContainer = document.querySelector(".result");
+// Text info showed after generate button is clicked
+const copyInfo = document.querySelector(".result__info.right");
+// Text appear after copy button is clicked
+const copiedInfo = document.querySelector(".result__info.left");
+
+const generateBtn = document.getElementById('generateBtn');
+
+// if this variable is trye only then the copyBtn will appear, i.e. when the user first click generate the copyBth will interact.
+let generatedPasswordFlag = false;
+
+generateBtn.addEventListener('click', () => {
+	const length = +slider.value;
+	const hasLower = lowercaseEl.checked;
+	const hasUpper = uppercaseEl.checked;
+	const hasNumber = numberEl.checked;
+	const hasSymbol = symbolEl.checked;
+	generatedPasswordFlag = true;
+
+	result_viewbox.innerText = generatePassword(length, hasLower, hasUpper, hasNumber, hasSymbol);
+}) 
---
+++ scr/js/_password_generator.js
@@ -0,0 +1,51 @@
+// Object of all the function names that we will use to create random letters of password
+const randomFunc = {
+	lower: getRandomLower,
+	upper: getRandomUpper,
+	number: getRandomNumber,
+	symbol: getRandomSymbol,
+};
+
+// Random more secure value
+function secureMathRandom() {
+	return window.crypto.getRandomValues(new Uint32Array(1))[0] / (Math.pow(2, 32) - 1);
+}
+
+// Generator Functions
+// All the functions that are responsible to return a random value taht we will use to create password.
+function getRandomLower() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomUpper() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNumber() {
+	return String.fromCharCode(Math.floor(secureMathRandom() * 10) + 48);
+}
+function getRandomSymbol() {
+	const symbols = '~!@#$%^&*()_+{}":?><;.,';
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+
+// Function responsible to generate password and then returning it.
+export function generatePassword(length, lower, upper, number, symbol) {
+
+
+	let generatedPassword = "";
+	const typesCount = lower + upper + number + symbol;
+
+	const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
+	if (typesCount === 0) {
+		return "";
+	}
+	for (let i = 0; i < length; i++) {
+		typesArr.forEach(type => {
+			const funcName = Object.keys(type)[0];
+			generatedPassword += randomFunc[funcName]();
+		});
+	}
+	return generatedPassword.slice(0, length)
+									.split('').sort(() => Math.random() - 0.5)
+									.join('');
+}
